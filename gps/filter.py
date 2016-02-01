488e12f48f14990adc51df8b0c87fbf856f25eec
Trever Hines <treverhines@gmail.com>
2016-02-01 01:08:27 -0500

    stochastic filter now allows for both jump estimation and restarts

--- gps/filter.py
+++ gps/filter.py
@@ -3,6 +3,11 @@ from __future__ import division
 import numpy as np
 import scipy.linalg
  
+def heaviside(t):
+  t = np.asarray(t)
+  return (t>=0.0).astype(float)
+
+
 def is_pos_def(C):
   '''
   Returns True if C is positive definite. 
@@ -66,13 +71,9 @@ def _rts_smooth(post,Cpost,prior,Cprior,smooth,Csmooth,F):
   return smooth_new,Csmooth_new
 
 def _stochastic_filter(u,var,t,alpha,
-                       init_prior=None,
-                       init_prior_cov=None,
+                       init_prior_var=1e3,
+                       jumps=None,
                        diff=0):
-  '''
-  Models the acceleration of the observation vector as white noise with 
-  variance alpha.
-  '''
   u = np.asarray(u)
   # turn u into an array of data vectors
   u = u[:,None]
@@ -80,30 +81,37 @@ def _stochastic_filter(u,var,t,alpha,
   # turn var into an array of covariance matrices
   var = var[:,None,None]
   t = np.asarray(t)
+  if jumps is None:
+    jumps = []
 
-  N = len(t)
-  # return empty arrays if the input is empty
+  # remove jumps that are not within the observation time interval
+  jumps = [j for j in jumps if (j>t[0]) & (j<t[-1])]
+  jumps = np.asarray(jumps)
+  
+  N = t.shape[0]
+  J = jumps.shape[0]
+  M = 2 + J
+
+  # return empty arrays if the time length is zero
   if N == 0:
-    return np.zeros((0,2)),np.zeros((0,2))
+    return np.zeros(0),np.zeros(0)
 
-  state_prior = np.zeros((N,2))
-  state_post = np.zeros((N,2))
-  state_prior_cov = np.zeros((N,2,2))
-  state_post_cov = np.zeros((N,2,2))
-  trans = np.zeros((N,2,2))
-  system = np.array([[1.0,0.0]])
+  state_prior = np.zeros((N,M))
+  state_post = np.zeros((N,M))
+  state_prior_cov = np.zeros((N,M,M))
+  state_post_cov = np.zeros((N,M,M))
+  trans = np.zeros((N,M,M))
 
-  if init_prior is None:
-    init_prior = np.zeros(2)
-
-  if init_prior_cov is None:
-    init_prior_cov = 1e3*np.eye(2)
-
-  state_prior[0,:] = init_prior
-  state_prior_cov[0,:,:] = init_prior_cov
+  # set initial prior covariance
+  state_prior_cov[0,:,:] = init_prior_var*np.eye(M)
 
   for i in range(N):
-    # incorperate observations to form posterior
+    system = np.zeros((1,M))
+    system[0,0] = 1.0
+    for j,val in enumerate(jumps):
+      system[0,2+j] = heaviside(t[i]-val)
+
+    # use observations to form posterior
     out = _update(system,u[i],var[i],state_prior[i],state_prior_cov[i])
     state_post[i] = out[0]
     state_post_cov[i] = out[1]
@@ -112,10 +120,13 @@ def _stochastic_filter(u,var,t,alpha,
     # Do not make a prediction for last time step
     if i != (N-1):
       dt = t[i+1] - t[i]
-      trans[i+1] = np.array([[1.0,      dt],
-                             [0.0,     1.0]])
-      trans_cov = np.array([[0.333*dt**3, 0.500*dt**2],
-                            [0.500*dt**2,          dt]])
+      trans[i+1] = np.eye(M)
+      trans[i+1,0,1] = dt
+      trans_cov = np.zeros((M,M))
+      trans_cov[0,0] = 0.333*dt**3 
+      trans_cov[0,1] = 0.500*dt**2 
+      trans_cov[1,0] = 0.500*dt**2 
+      trans_cov[1,1] = dt
       trans_cov *= alpha**2
 
       out = _predict(trans[i+1],trans_cov,state_post[i],state_post_cov[i])
@@ -123,8 +134,8 @@ def _stochastic_filter(u,var,t,alpha,
       state_prior_cov[i+1] = out[1]
     
   # smooth the state variables 
-  state_smooth = np.zeros((N,2))
-  state_smooth_cov = np.zeros((N,2,2))
+  state_smooth = np.zeros((N,M))
+  state_smooth_cov = np.zeros((N,M,M))
   state_smooth[-1] = state_post[-1]
   state_smooth_cov[-1] = state_post_cov[-1]
 
@@ -148,36 +159,88 @@ def _stochastic_filter(u,var,t,alpha,
 
   # returns the indicated derivative
   out = state_smooth[:,diff]
-  out_cov = state_smooth_cov[:,diff,diff]
-  return out,out_cov
+  out_var = state_smooth_cov[:,diff,diff]
+  return out,out_var
 
 
-def stochastic_filter(u,var,t,alpha,jumps=None,init_prior=None,init_prior_cov=None,diff=None):
-  ''''
-  independent Kalman filters are applied to each time interval between jumps
+def stochastic_filter(u,var,t,alpha,
+                      restarts=None,
+                      jumps=None,
+                      init_prior_var=1e3,
+                      diff=0):
+  '''
+  Description
+  -----------
+    Uses a Kalman filter to estimate a smooth underlying signal from
+    noisy, and possibly discontinous, data.
+
+    The acceleration of the underlying signal is modeled as white
+    noise with standard deviation alpha. A Kalman filter is used to
+    estimate the underlying signal and also estimate magnitudes of
+    jumps at known times.
+  
+    The observation function is
+   
+      u_obs(t) = u_true(t) + a_0*H(t-t_0) + ... a_J*H(t-t_J) + sigma
+  
+    
+    where sigma is is the observation noise, H(t) is the Heaviside
+    function, t_i are the jump times, a_i are the jump magnitudes that
+    will be estimates. u_true(t) is the true signal we are trying to
+    estimate, which is modeled as a stochastic process.
+
+    Our state vector consists of the true signal, its time 
+    derivative and the jump coefficients:
+
+      X(t) = [u_true(t), u_true'(t), a_0, ... a_J]. 
+
+    The state transition function is 
+   
+      X(t+1) = F(dt)*X(t) + epsilon
+
+    where epsilon is the process noise and 
+
+      F(dt) = [1.0  dt 0.0     0.0]
+              [0.0 1.0 0.0 ... 0.0] 
+              [0.0 0.0 1.0     0.0]
+              [     :          :  ]
+              [0.0 0.0 0.0 ... 1.0].
+
+    epsion has zero mean and covariance described by
+
+      Q(dt) = [dt**3/3 dt**2/2 0.0 ... 0.0]   
+              [dt**2/2      dt 0.0 ... 0.0]
+              [0.0         0.0 0.0     0.0]       
+              [     :                  :  ]
+              [0.0         0.0 0.0 ... 0.0].
+
+  Returns
+  -------
+    the estimate of u_true and its variance or the time derivative 
+    of u_true and its variance. 
+
   '''
   u = np.asarray(u)
   var = np.asarray(var)
   t = np.asarray(t)
 
-  if jumps is None:
-    jumps = np.zeros(0,dtype=float)
+  if restarts is None:
+    restarts = np.zeros(0,dtype=float)
 
-  jumps = np.sort(jumps)
-  jumps = np.concatenate(([-np.inf],jumps,[np.inf]))
-  N = len(jumps)
-  out = np.zeros(len(t))
-  out_cov = np.zeros(len(t))
-  for i in range(N-1):
-    idx = (t > jumps[i]) & (t <= jumps[i+1])  
+  restarts = np.sort(restarts)
+  restarts = np.concatenate(([-np.inf],restarts,[np.inf]))
+  out = np.zeros(t.shape[0])
+  out_var = np.zeros(t.shape[0])
+  for i in range(restarts.shape[0]-1):
+    idx = (t >= restarts[i]) & (t < restarts[i+1])  
     a,b = _stochastic_filter(u[idx],var[idx],t[idx],alpha,
-                             init_prior=init_prior,
-                             init_prior_cov=init_prior_cov,
+                             jumps=jumps,
+                             init_prior_var=init_prior_var,
                              diff=diff)
     out[idx] = a
-    out_cov[idx] = b
+    out_var[idx] = b
 
-  return out,out_cov
+  return out,out_var
 
 
 def running_mean(u,var,t,Ns=10,jumps=None):
